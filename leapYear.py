b81e1ad03223d1d01ddb0edd7917705b5e8f7760
David Cronk <cronkd@oregonstate.edu>
2021-03-07 19:09:39 -0800

    Added code for 4th test of leapYear - added code prints that years divisible by 400 are leap year - code is complete

--- leapYear.py
+++ leapYear.py
@@ -1,7 +1,10 @@
 def leap(year):
 	if (year % 4) == 0:
 		if (year % 100) == 0:
-			print(str(year) + " is not a leap year!\n")
+			if (year % 400) == 0:
+				print(str(year) + " is a leap year!\n")
+			else:
+				print(str(year) + " is not a leap year!\n")
 		else:
 			print(str(year) + " is a leap year!\n")
 	else:
