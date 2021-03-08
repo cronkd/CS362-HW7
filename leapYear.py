fe77ceb801eb9431aeb6a6f3b53e9828aac12ae5
David Cronk <cronkd@oregonstate.edu>
2021-03-07 19:05:02 -0800

    added code for 3rd test of leapYear - prints not leap year for years diviible by 100

--- leapYear.py
+++ leapYear.py
@@ -1,5 +1,8 @@
 def leap(year):
 	if (year % 4) == 0:
-		print(str(year) + " is a leap year!\n")
+		if (year % 100) == 0:
+			print(str(year) + " is not a leap year!\n")
+		else:
+			print(str(year) + " is a leap year!\n")
 	else:
 		print(str(year) + " is not a leap year!\n")
