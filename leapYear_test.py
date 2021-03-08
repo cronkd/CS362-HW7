d599c7ffdee1970a419575067f53840529273f82
David Cronk <cronkd@oregonstate.edu>
2021-03-07 19:00:19 -0800

    Added 3rd test to leapYear - testing if years divisible by 100 print not leap year

--- leapYear_test.py
+++ leapYear_test.py
@@ -23,6 +23,16 @@ class TestCase(unittest.TestCase):
 		output = sys.stdout.getvalue().strip()
 		#compares stdout with that it's supposed to be
 		self.assertEqual(output,'2017 is not a leap year!')
+	def test3(self):
+		#calls print function of program
+		leapYear.leap(1500)
+		#check if using buffer
+		if not hasattr(sys.stdout,"getvalue"):
+			self.fail("need to run in buffered mode")
+		#save stdout as ouput
+		output = sys.stdout.getvalue().strip()
+		#compares stdout with that it's supposed to be
+		self.assertEqual(output,'1500 is not a leap year!')
 if __name__ == '__main__':
 	assert not hasattr(sys.stdout,"getvalue")
 	unittest.main(module=__name__, buffer = True,exit=False)
